f237c61ced8ff10e2842995cc991ac0d860bf83c
Avinash H. Duduskar <2946372+Strykar@users.noreply.github.com>
2023-05-01 20:29:16 +0530

    Create aws_update_ipv6.sh

---
+++ .bin/aws_update_ipv6.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Script to update the AAAA RR of V6_URL below using aws-cli-v2 to the current IP on V6_INT
+# Assumes AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment vars exist or export them
+
+V6_INT=enp2s0
+RR_TTL=60
+RR_TYPE=AAAA
+RR_ACTION=UPSERT
+AWS_REGION=xxx
+V6_URL=xxx
+ZONE_ID=xxx
+#export AWS_ACCESS_KEY_ID=xxx
+#export AWS_SECRET_ACCESS_KEY=xxx
+
+# Delete any 6Bone / Teredo IPs if the router assigns one from the default broken (when IPv4 is static) radvd.conf
+ip -6 addr | awk '$2 ~ /^3ffe:/ {system("sudo ip -6 addr del " $2 " dev enp2s0")}'
+
+# Get IPv6 GUA 
+IPV6_IP=$(ip -6 addr show dev ${V6_INT} scope global | awk '/inet6/ {split($2, a, "/"); print a[1]}')
+
+if [[ "$(dig +short -r AAAA ${V6_URL})" == "${IPV6_IP}" ]]; then
+	echo "$(/usr/bin/date): [NOK]  NOCHANGE: AWS_R53 AAAA RR is correct, exiting."
+	exit 0
+else
+	echo "$(/usr/bin/date): [AWS_CLIv2]  UPSERT: Stale IPv6 address [$(dig +short -r AAAA ${V6_URL})] detected, updating Route53.."
+	/usr/bin/aws route53 change-resource-record-sets \
+    --hosted-zone-id "${ZONE_ID}" \
+    --change-batch '{
+        "Changes": [
+            {
+                "Action": "'"${RR_ACTION}"'",
+                "ResourceRecordSet": {
+                    "Name": "'"${V6_URL}"'",
+                    "Type": "'"${RR_TYPE}"'",
+                    "TTL": '"${RR_TTL}"',
+                    "ResourceRecords": [
+                        {
+                            "Value": "'"${IPV6_IP}"'"
+                        }
+                    ]
+                }
+            }
+        ]
+    }' \
+    --region "${AWS_REGION}" \
+    --cli-connect-timeout 5 \
+    --query 'ChangeInfo.Id' \
+    --output text
+fi
